e3d364ddc320d59860b89c94b07ba30b6e53a494
Zain🍕 <zainadam.id@gmail.com>
2025-11-24 02:02:00 +0800

    Supports URLs with hash fragments
    Updates the URL regex to correctly validate URLs that include hash fragments (e.g., `#section`). Previously, URLs containing a hash character were incorrectly marked as invalid.

--- src/side-panel/components/address-bar/utils.ts
+++ src/side-panel/components/address-bar/utils.ts
@@ -6,7 +6,7 @@ export function cleanUrl(url: string): string {
 }
 
 export const URL_REGEX =
-    /^(https?:\/\/)?(([\da-z.-]+)\.([a-z.]{2,})|localhost|(\d{1,3}\.){3}\d{1,3})(:[0-9]+)?([/\w\s.\-?&=%+]*)\/?$|^chrome:\/\/[\w-]+([/\w.-]*)*\/?$|^chrome-extension:\/\/[\w]+([/\w.-]*)*\/?$/
+    /^(https?:\/\/)?(([a-z\d.-]+)\.([a-z.]{2,})|localhost|(\d{1,3}\.){3}\d{1,3})(:[0-9]+)?([/\w\s.\-?&=%+#]*)\/?$|^chrome:\/\/[\w-]+([/\w.-]*)*\/?$|^chrome-extension:\/\/[\w]+([/\w.-]*)*\/?$/
 
 export function isValidUrl(href: string): boolean {
     return URL_REGEX.test(href)
